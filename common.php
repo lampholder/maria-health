ed285291b4efaef85db5e429fef96c8bd0d50007
Tom Lant <lampholder@gmail.com>
2017-01-16 20:45:14 -0500

    Less stupid defaults

--- common.php
+++ common.php
@@ -1,7 +1,10 @@
 <?php
 
-$brand = 'WATSIT';
-$title = 'Websites are good.';
+$brand = 'Think Active!';
+$title = 'Get active in your local community.';
 $sitename = 'SITENAME';
+
+$user = 'Julian';
+$region = 'Putney';
 
 ?>
